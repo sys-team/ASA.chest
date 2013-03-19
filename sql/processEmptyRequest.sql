b5e7ee0db3c19a14bd42ef4671300e0b18c5bc30
alevinru <alevin.ru@gmail.com>
2013-03-19 09:15:08 +0400

    STGT serttings

--- sql/processEmptyRequest.sql
+++ sql/processEmptyRequest.sql
@@ -4,32 +4,62 @@ begin
 
     declare @result xml;
     
-    set @result = (select top 1 xmlelement('d'
+    set @result = xmlelement('d'
         , xmlattributes ('STGTSettings' as "name")
         , xmlconcat(
             
-            (select xmlagg(
-                    xmldatum
-                )
-                from openxml(xmlData,'*/*') with (
-                    name varchar(32) '@name'
-                    , xmldatum xml '@mp:xmltext'
-                ) where name not in (
-                    'ts'
-                )
-            ) 
+            (select top 1 (
+                select xmlagg(xmldatum)
+                    from openxml(xmlData,'*/*') with (
+                        name varchar(32) '@name'
+                        , xmldatum xml '@mp:xmltext'
+                    ) where name not in (
+                        'ts', 'distanceFilter', 'requiredAccuracy', 'timeFilter', 'syncInterval', 'fetchLimit', 'syncServerURI'
+                    )
+                    
+                ) as xmlData
+                
+                from ch.entity
+                where name = 'STGTSettings'
+                order by id
+            )
+            
             , xmlelement(
                 'date'
                 , xmlattributes ('ts' as "name")
                 , left(current utc timestamp, 19) + ' +0000'
             )
-            
-        )) as xmlData
-        
-        from ch.entity
-        where name = 'STGTSettings'
-        order by id
-    );
+            , xmlelement(
+                'double'
+                , xmlattributes ('distanceFilter' as "name")
+                , 20
+            )
+            , xmlelement(
+                'double'
+                , xmlattributes ('timeFilter' as "name")
+                , 15
+            )
+            , xmlelement(
+                'double'
+                , xmlattributes ('requiredAccuracy' as "name")
+                , 20
+            )
+            , xmlelement(
+                'double'
+                , xmlattributes ('fetchLimit' as "name")
+                , 20
+            )
+            , xmlelement(
+                'double'
+                , xmlattributes ('syncInterval' as "name")
+                , 240
+            )
+            , xmlelement(
+                'string'
+                , xmlattributes ('syncServerURI' as "name")
+                , 'https://system.unact.ru/asa/?_host=asa0&_svc=chest'
+            )
+    ));
     
     return @result;
     
