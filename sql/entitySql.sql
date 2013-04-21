8929b8b5b900d3b134ac767b08ce9fc48c003f17
alevinru <alevin.ru@gmail.com>
2013-04-21 20:04:00 +0400

    убрал remotexid + формат

--- sql/entitySql.sql
+++ sql/entitySql.sql
@@ -6,40 +6,49 @@ begin
     declare @sql long varchar;
     declare @cnt integer;
     
-    set @sql = (select list(f)
-                  from (
-                select    
+    set @sql = (
+        select list(f)
+        from (
+            select    
                 'select e.id, e.version, e.author, e.xid, e.ts, e.cts ' as f
-                union
-                select nullif(
+            union select nullif(
                 (select list('(select c.xid as [' + r.name + '] '+
                         'from ch.relationship r join ch.entity c on r.child = c.id ' +
                         ' where r.parent = e.id and c.name = ''' + r.actor +''') ')
-                   from ch.entityRole r
-                  where entity = @entity),'')) as t);
-                  
-    set @cnt = (select count(*)
-                  from ch.entityProperty
-                 where entity = @entity);
-                  
+                    from ch.entityRole r
+                    where entity = @entity
+                ),
+                ''
+            )
+        ) as t
+    );
+    
+    set @cnt = (
+        select count(*)
+        from ch.entityProperty
+        where entity = @entity
+    );
+    
     set @sql = @sql +
-                if @cnt <> 0
-                 then ',' +
-                      (select list('x.[' + ch.remoteColumnName(property) + ']') +', remoteXid'
-                        from ch.entityProperty where entity = @entity)
-                 else '' endif +
-                ' from ch.entity e ' +
-                if @cnt <> 0 then 
-                    ' outer apply (select ' +
-                    (select list('[' + ch.remoteColumnName(property) + ']') +', remoteXid'
-                      from ch.entityProperty where entity = @entity) +
-                    ' from openxml(e.xmlData, ''/*:d'') with(' +
-                    (select list('[' + ch.remoteColumnName(property) + '] long varchar ''*[@name="' + property + '"]''')
-                       from ch.entityProperty where entity = @entity) +
-                    ', remoteXid long varchar ''@xid'')) as x ' 
-                else '' endif +
-                ' where e.name = ''' + @entity +'''';
+        if @cnt <> 0
+            then ',' +
+              (select list('x.[' + ch.remoteColumnName(property) + ']')
+                from ch.entityProperty where entity = @entity)
+            else ''
+        endif +
+        ' from ch.entity e ' +
+        if @cnt <> 0 then 
+            ' outer apply (select ' +
+            (select list('[' + ch.remoteColumnName(property) + ']')
+              from ch.entityProperty where entity = @entity) +
+            ' from openxml(e.xmlData, ''/*:d'') with(' +
+            (select list('[' + ch.remoteColumnName(property) + '] long varchar ''*[@name="' + property + '"]''')
+               from ch.entityProperty where entity = @entity) +
+            ', remoteXid long varchar ''@xid'')) as x ' 
+        else '' endif +
+        ' where e.name = ''' + @entity +''''
+    ;
     
     return @sql;
-end
-;
+    
+end;
