65d20ca52fdb8260637731d5cb1d0c1f80faae41
alevinru <alevin.ru@gmail.com>
2013-04-21 19:49:36 +0400

    defineProperties

---
+++ sql/defineProperties.sql
@@ -0,0 +1,12 @@
+create or replace procedure ch.defineProperties (
+    @data text
+) begin
+
+    insert into ch.property on existing update with auto name
+        select (select id from ch.property where name = cols.name) as id, *
+        from openstring(
+            value @data
+        ) with ([name] text, [type] text) option(ROW DELIMITED BY ':') as cols
+    ;
+
+end;
