dab29ccd9257812ab94a400f9e8fa5aa6a8c482a
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-04-24 15:42:38 +0400

    persistData

--- sql/persistData.sql
+++ sql/persistData.sql
@@ -1,9 +1,10 @@
 create or replace procedure ch.persistData(
     @startTs datetime,
-    @owner varchar(128) default 'ch'
+    @owner varchar(128) default 'ch',
     @entity long varchar default null
 )
 begin
+    declare @sql long varchar;
 
     for lloop as ccur cursor for
     select distinct
@@ -13,7 +14,22 @@ begin
         or @entity is null)
     do
         if exists(select *
-                    from sys.systable)
+                    from sys.systable t join sys.sysuserperm u on t.creator = u.user_id
+                   where t.table_name = c_entity
+                     and u.user_name = @owner
+                     and t.table_type in ('BASE', 'GBL TEMP')) then
+                     
+            set @sql = 'insert into [' + @owner + '].[' + c_entity + '] on existing update with auto name ' +
+                       ch.entitySql(c_entity) +
+                       ' and e.ts >= ''' + cast(@startTs as varchar(24)) +'''';
+                       
+            -- message 'ch.persistData @sql = ', @sql;
+            
+            execute immediate @sql;
+            
+            commit;
+                     
+        end if;
 
     end for;
 
