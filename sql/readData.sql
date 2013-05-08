2e9d98c12deb416cfd31e119510989ab4ddfc0ac
alevinru <alevin.ru@gmail.com>
2013-05-08 07:43:21 +0400

    Форматирование и подготовка к работе без xid

--- sql/readData.sql
+++ sql/readData.sql
@@ -5,58 +5,77 @@ begin
     
     -- entities
     insert into #entity with auto name
-    select util.strtoxid(xid) as xid,
-           name,
-           xmlData
-      from openxml(@request, '/*/*:d')
-           with(xid long varchar '@xid', name long varchar '@name', xmlData xml '@mp:xmltext');
-           
+        select
+            isnull(util.strtoxid(xid),newid()) as xid,
+            name,
+            xmlData
+        from openxml(@request, '/*/*:d') with (
+            xid long varchar '@xid',
+            name long varchar '@name',
+            xmlData xml '@mp:xmltext'
+        )
+    ;
+    
     --message 'ch.readData #1';
-           
+    
     -- entities from rel
     insert into #entity with auto name
-    select distinct util.strtoxid(xid) as xid,
-           name
-      from openxml(@request, '/*/*:d/*:d')
-      with(xid long varchar '@xid', name long varchar '@name', xmlData xml '@mp:xmltext') as t
-     where not exists(select *
-                        from #entity
-                       where xid = util.strtoxid(t.xid))
-       and not exists (select *
-                         from ch.entity
-                        where xid =  util.strtoxid(t.xid));
-                       
+        select distinct
+            isnull(util.strtoxid(xid),newid()) as xid,
+            name,
+            xmlData
+        from openxml(@request, '/*/*:d/*:d') with (
+                xid long varchar '@xid',
+                name long varchar '@name',
+                xmlData xml '@mp:xmltext'
+            ) as t
+        where not exists( select *
+            from #entity
+            where xid = util.strtoxid(t.xid)
+        ) and not exists( select *
+            from ch.entity
+            where xid =  util.strtoxid(t.xid)
+        )
+    ;
+    
     --message 'ch.readData #2';
     
     -- attributes
     insert into #attribute with auto name
-    select name,
-           dataType,
-           value,
-           xmlData,
-           parentXid,
-           parentName
-      from openxml(@request, '/*/*:d/*')
-           with(
-                    name long varchar '@name', 
-                    dataType long varchar '@mp:localname', 
-                    value long varchar '.',
-                    xmlData xml '@mp:xmltext',
-                    parentXid long varchar '../@xid',
-                    parentName long varchar '../@name'
-                )
-     where dataType <> 'd';
-           
+        select
+            c.name,
+            c.dataType,
+            c.value,
+            c.xmlData,
+            e.xid parentXid,
+            e.name parentName
+        from #entity as e cross apply ( select *
+            from openxml(e.xmldata, '/*/*') with (
+                name long varchar '@name', 
+                dataType long varchar '@mp:localname', 
+                value long varchar '.',
+                xmlData xml '@mp:xmltext'
+            ) where dataType <> 'd'
+        ) as c 
+    ;
+    
     -- rels
     insert into #rel with auto name
-    select util.strtoxid(xid) as childXid,
-           util.strtoxid(parentXid) as parentXid,
-           name,
-           xmlData
-      from openxml(@request, '/*/*:d/*:d')
-           with(xid long varchar '@xid', parentXid long varchar '../@xid' ,name long varchar '@name', xmlData xml '@mp:xmltext');
-           
+        select
+            util.strtoxid(c.xid) as childXid,
+            e.xid as parentXid,
+            c.name,
+            c.xmlData
+        from #entity as e cross apply ( select *
+            from openxml(e.xmldata, '/*/*:d') with (
+                xid long varchar '@xid',
+                name long varchar '@name',
+                xmlData xml '@mp:xmltext'
+            )
+        ) as c
+    ;
+    
     --message 'ch.readData #3';
-           
+    
 end
 ;
