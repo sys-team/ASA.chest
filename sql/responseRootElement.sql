ff1b9bc53909052f507cc290f15423579f076dda
Alexey Panshin <alexey.i.panshin@gmail.com>
2013-05-28 08:43:44 +0400

    Revert "Revert "Revert "xml-prolog in response"""
    This reverts commit 8e910198020db2c4d6290a560f9cd1882d027a69.

--- sql/responseRootElement.sql
+++ sql/responseRootElement.sql
@@ -5,8 +5,7 @@ returns xml
 begin
     declare @result xml;
     
-    set @result = '<?xml version="1.0" encoding="windows-1251"?>' +
-                  xmlelement('response', xmlattributes('https://github.com/sys-team/ASA.chest' as "xmlns",now() as "ts"), @response);
+    set @result = xmlelement('response', xmlattributes('https://github.com/sys-team/ASA.chest' as "xmlns",now() as "ts"), @response);
     
     return @result;
 end
