bb0115f74dffbfa450119bbafe73e2e42910f8e9
Alexander Levin <alevin.ru@gmail.com>
2013-09-19 22:03:19 +0400

    trigger named entity events

--- sql/triggerEvent.sql
+++ sql/triggerEvent.sql
@@ -1,16 +1,46 @@
-create or replace procedure ch.triggerEvent()
-begin
+create or replace procedure ch.triggerEvent(
+    @code STRING default null
+) begin
     declare @sql long varchar;
 
-    for lloop as ccur cursor for 
+    for lloop as global_events cursor for 
     select event_name as c_name
-      from sys.sysevent
-     where event_name like '%chestDataPersist'
-       and enabled = 'Y'
+        from sys.sysevent
+        where event_name like '%chestDataPersist'
+            and enabled = 'Y'
     do
         set @sql  ='trigger event ' + c_name;
         execute immediate @sql;
     end for;
+    
+    begin
+        
+        DECLARE no_entity EXCEPTION FOR SQLSTATE '42W33';
+        
+        for lloop as entity_events cursor for 
+        select
+                ev.event_name as event_name,
+                en.name as entity_name,
+                list (en.xid) as entity_xids
+            from sys.sysevent ev join #entity en
+                on ev.event_name like en.name+'%chestEntityPersist'
+            where ev.enabled = 'Y'
+            group by event_name, entity_name
+        do
+            set @sql = string (
+                'trigger event ', event_name,
+                ' ( ',
+                    '"entity_xids" = ''', entity_xids, '''',
+                    ',"auth_token" = ''', isnull (@code, 'null'), '''',
+                ' )'
+            );
+            message 'ch.triggerEvent:', @sql to client;
+            execute immediate @sql;
+        end for;
+        
+    exception when no_entity then
+        message 'ch.triggerEvent:', 'No #entity ' to client ;
+        
+    end;
 
-end
-;
+end;
