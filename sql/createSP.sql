fba50995523f97959f40620d4ed59c0a701e8dd9
Alexander Levin <alevin.ru@gmail.com>
2014-10-16 13:17:20 +0300

    @owner in ch.createSP

--- sql/createSP.sql
+++ sql/createSP.sql
@@ -1,6 +1,7 @@
 create or replace procedure ch.createSP(
     @entity string default null,
-    @entitySrc string default null
+    @entitySrc string default null,
+    @owner string default 'ch'
 )
 begin
     declare @sql long varchar;
@@ -13,7 +14,7 @@ begin
         or @entity is null)
     union select @entity
     do
-        set @sql = 'create or replace procedure ch.'
+        set @sql = 'create or replace procedure [' + @owner + '].'
             + '[' + c_name + ']'
             + '() begin '
             + ch.entitySql(c_name, 0, isnull(@entitySrc, c_name))
