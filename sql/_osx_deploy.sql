fdb5589c4d705c3aecf15d34dcb38762a997a0b5
DeadCat93 <alexey.i.panshin@gmail.com>
2014-10-31 09:32:54 +0300

    _osx_deploy.sql

---
+++ sql/_osx_deploy.sql
@@ -0,0 +1,32 @@
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/_tables.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/chest.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/chest_service.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/createSP.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/createTable.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/createView.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/defineEntity.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/defineProperties.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/entityData.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/entityMaxTS.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/entitySql.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/entityStats.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/entityWriteable.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/fillEntityProperty.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/fillEntityRole.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/get.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/makeAnswer.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/mergeXml.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/parseVariables.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/persistData.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/persistData_event.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/processEmptyRequest.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/readData.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/remoteColumnName.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/responseRootElement.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/saveAccessToken.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/saveData.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/temporaryTables.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/triggerEvent.sql;
+read /Users/%YourUserNameHere%/Sites/ASA.chest/sql/triggers.sql;
+
+
